62a2f87df6e023d3fcaf68f1eb0c5b2138c5cb64
Louise Moraldy <louisemoraldy@proton.me>
2026-04-07 13:35:59 +0200

    app.js setting

--- app.js
+++ app.js
@@ -2,32 +2,52 @@
 //       APP.JS
 // -------------------
 
-require('dotenv').config() // chargement des variables d'environnement 
-
+// ! 1) Importer express + Créer le serveur
 const express = require('express'); //import d'express
 const server = express(); //création du serveur express
+const mongoose = require("mongoose"); // import mongoose
 
 
-// get sur localhost:3000
-// Création d'une route de test (GET sur la racine)
-server.get('/', (req, res) => {
-    res.status(200).json({ 
-        message: "Bienvenue sur l'API de Social Script 🚀",
-        status: "En ligne"
-    });
-});
+// ? Récupération des variables d'environnement :
+const { PORT, DB_CONNECTION } = process.env;
 
-// get sur localhost:3000/scenarios
-server.get('/scenarios', (req, res) => {
+// ? Pour paramétrer le fait que notre API doit comprendre quand du json arrive
+server.use(express.json());
 
-    res.send({ message : 'Voici tous les scenarios'}, 200);
+// ?  2/ Mongoose Middleware
+//création Middleware qui ... Mongoose
+//pour établir la connexion besoin d'importer Mongosse (cf plus haut ligne  require("mongoose");)
+server.use(async (req, res, next) => {
+    // a partir de cet objet nous pouvons essayer d'établir une connexion
+    // ici on essaye de se connecter mais ça peut prendre du temps ou echouer donc la méthode pour se connecter nous envoie une promisse. Il faut donc utiliser then catch  soit le asyn await avec le try catch (plus propre)
+    try {
+        // on essaye de se connecter
+        await mongoose.connect(DB_CONNECTION, { dbName: 'TaskManager' }); // Pouet = une URL bidon
+        console.log("💾 Successfully log to the DB 🥳");
+        next(); //si ça  fonctionné on continue la requète
+
+    } catch (err) {
+        // si ça  ne marche pas on écrit dans la consolole un mess d'erreur
+        console.log(`🙅⛔Connection failed \n[Reason]\n ${err}`);
+        res.status(500).json({ statusCode: 500, message: 'Impossible de connecter à la DB ☹️' })
+    }
 })
 
-const router = require("./routes"); // on indique que notre serveur Express doit utiliser ce routeur
-server.use('/api', router);
+
+// ! 2) Traiter les requêtes
+// indiquer à notre app que le routing se trouve dans le dossier 📁 routes
+const router = require('./routes'); //import de l'objet routeur présent dans index.js
+server.use('/api', router); //indiquer à notre server qu'il doit utiliser le router
 
 
-// Écouter le serveur sur un port spécifique
-server.listen(3000, () => {
-    console.log(`🚀 Serveur SocialScript démarré sur le port ${ 3000 }`);
+// ! 3) Écouter le serveur sur un port spécifique
+server.listen(PORT, () => {
+    console.log(`🚀 Serveur SocialScript démarré sur le port ${PORT}`);
 })
+
+
+// * dot.env --> Librarie pas aussi moderne que la fonctionnalité native de Node
+
+// Plus tard
+// ajouter app level middelware / du middleware CORS
+// --> cf. demo ligne 18
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
-// Rappel cours : 📁 routes : définition de toutes les routes de notre API (verb + url statique + params)
+// ** Index.js
+// Point d'entrée de toute nos routes
+// Role : définition de toutes les routes de notre API (verb + url statique + params)
+
+
+// ! 1) Créer un objet "routeur" (router)
 const router = require('express').Router();
+
+
+// ! 2) Configurer les routes
+// permet d'indiquer que notre router de base doit utiliser, si l'url est localhost:3000/api/quelque_chose, le quelque_chose routeur pour la suite
 
 router.get('/', (req, res) => {
     res.send("Bienvenue sur l'API de Social Script", 200)
